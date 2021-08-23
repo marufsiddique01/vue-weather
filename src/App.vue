43759ce2586a9cac5f2ac70f519a3050a65fb994
Maruf Siddique <maruf.siddique@northsouth.edu>
2021-08-23 22:45:42 +0600

    backgrounds and cse of search bar and search box

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,79 @@
 <template>
   <div id="app">
-    
+    <main>
+      <div class="search-box">
+        <input type="text" name="" class="search-bar" placeholder="Search..." />
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  
-}
+  name: "App",
+  data() {
+    return {
+      api_key: "a11c90869de113dcc70c13800ab7850d",
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  font-family: "monserrat", sans-serif;
+}
+
+#app {
+  background-image: url("./assets/cold.jpg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0.75)
+  );
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #070707;
+  font-size: 30px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 15px 0px 15px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
 </style>
